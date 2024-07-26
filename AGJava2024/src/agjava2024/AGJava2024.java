81de96d52c25233171d0b5f4a8328f0d7c53d717
Leonardo Fragoso <84422507+LeoSIIF@users.noreply.github.com>
2024-07-25 21:34:44 -0300

    Update AGJava2024.java

--- AGJava2024/src/agjava2024/AGJava2024.java
+++ AGJava2024/src/agjava2024/AGJava2024.java
@@ -1,16 +1,18 @@
 package agjava2024;
 
 public class AGJava2024 {
+
     public static void main(String[] args) {
         int populacao = 20;
-        double capacidadePeso = 400;
-        double larguraMaxima = 300;
-        double alturaMaxima = 300;
-        double profundidadeMaxima = 400;
-        int probabilidadeMutacao = 5; // 5%
+        double capacidadePeso = 400;  
+        double larguraMaxima = 300; 
+        double alturaMaxima = 300;    
+        double profundidadeMaxima = 400;  
+        int probabilidadeMutacao = 5; 
         int qtdeCruzamentos = 5;
-        AlgoritmoGenetico meuAg = new AlgoritmoGenetico(
-                populacao, capacidadePeso, larguraMaxima, alturaMaxima, profundidadeMaxima, probabilidadeMutacao, qtdeCruzamentos);
+        int numeroGeracoes = 10;
+
+        AlgoritmoGenetico meuAg = new AlgoritmoGenetico(populacao, capacidadePeso, larguraMaxima, alturaMaxima, profundidadeMaxima, probabilidadeMutacao, qtdeCruzamentos, numeroGeracoes);
         meuAg.carregaArquivo("dados.csv");
         meuAg.executar();
     }
